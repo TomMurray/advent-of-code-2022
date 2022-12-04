842f906c0be9d43f8abedbb4d5c0974d024f9be7
Tom Murray <tom.paul.murray@gmail.com>
2022-12-04 11:33:17 +0000

    As I'm working through Rust tutorials, open the input data file.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+  const PATH : &str = "input.txt";
+  let _input_file = match File::open(PATH) {
+    Ok(file) => { println!("Successfully opened {}", PATH); file},
+    Err(error) => panic!("Could not open {}: {:?}", PATH, error)
+  };
 }
