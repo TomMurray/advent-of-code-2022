6fde32783ce41e5668e6c866d25b74da7895db82
Tom Murray <tom.paul.murray@gmail.com>
2022-12-04 11:36:48 +0000

    A simpler hackier way to just error if the file wasn't opened

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,8 +2,5 @@ use std::fs::File;
 
 fn main() {
   const PATH : &str = "input.txt";
-  let _input_file = match File::open(PATH) {
-    Ok(file) => { println!("Successfully opened {}", PATH); file},
-    Err(error) => panic!("Could not open {}: {:?}", PATH, error)
-  };
+  let _input_file = File::open(PATH).unwrap();
 }
