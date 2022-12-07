9eaa17366db30c7e1d0295f60cf961e44007e1d3
Tom Murray <tom.paul.murray@gmail.com>
2022-12-07 16:15:28 +0000

    Implement day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -34,6 +34,10 @@ impl<'a> Rucksack<'a> {
         }
     }
 
+    fn get_item_mask(&self) -> u64 {
+        get_item_mask(self.s)
+    }
+
     fn find_duplicate(&self) -> Option<u8> {
         let lhs : &str = &self.s[0..self.s.len() / 2];
         let rhs : &str = &self.s[(self.s.len() / 2)..self.s.len()];
@@ -52,6 +56,8 @@ impl<'a> Rucksack<'a> {
     }
 }
 
+const ELVES_IN_GROUP : usize = 3;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args : Vec<String> = env::args().collect();
     let path : &str = &args[1];
@@ -59,16 +65,28 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let lines = BufReader::new(input_file).lines();
     
-    let mut total_prios : u64 = 0;
+    let mut total_dup_prios : u64 = 0;
+    let mut total_badge_prios : u64 = 0;
+    let mut group_count = 0;
+    let mut curr_badge_mask = !0u64;
     for line in lines {
         if let Ok(l) = line {
             // Process each rucksack
             let sack = Rucksack::new(&l);
             let dup = sack.find_duplicate().expect("should always be a duplicate in a valid sack");
-            let prio : u64 = to_priority(dup).into();
-            total_prios += prio;
+            let dup_prio : u64 = to_priority(dup).into();
+            total_dup_prios += dup_prio;
+            curr_badge_mask &= sack.get_item_mask();
+            group_count = (group_count + 1) % ELVES_IN_GROUP;
+            if group_count == 0 {
+                let badge_prio : u64 = curr_badge_mask.trailing_zeros().into();
+                total_badge_prios += badge_prio;
+                curr_badge_mask = !0u64;
+            }
         }
     }
-    println!("Total of priorities of duplicated sack items was {}", total_prios);
+    assert!(group_count == 0);
+    println!("Total of priorities of duplicated sack items was {}", total_dup_prios);
+    println!("Total of priorities of badges in each elf group was {}", total_badge_prios);
     Ok(())
 }
