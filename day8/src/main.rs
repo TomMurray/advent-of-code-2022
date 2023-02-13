fd78effe63fbb561141933b138a8c94402073380
Tom Murray <tom.paul.murray@gmail.com>
2023-02-13 11:46:31 +0000

    day 8 part 1 working finally

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,8 +1,24 @@
-use std::{fmt::{Debug, self}, fs::{File, self}, error::Error, env, str::FromStr};
+use std::{fmt::{self}, fs::{self}, error::Error, env, iter::once, ops::{Index, Mul}};
+use bit_set::BitSet;
+use itertools::Itertools;
 
 struct Matrix<T> {
     storage : Vec<T>,
     inner_dim : usize
+}
+
+impl<T> Matrix<T> {
+    fn width(&self) -> usize {
+        self.inner_dim
+    }
+
+    fn height(&self) -> usize {
+        self.storage.len() / self.inner_dim
+    }
+    
+    fn num_elements(&self) -> usize {
+        self.storage.len()
+    }
 }
 
 impl Matrix<u32> {
@@ -31,7 +47,61 @@ impl<T> fmt::Display for Matrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "  TODO: fmt::Display for Matrix<T>")
     }
-} 
+}
+
+impl<T, Idx> Index<Idx> for Matrix<T>
+where
+    Idx : Mul + Into<usize> {
+    type Output = [T];
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        let idx_cast: usize = index.into();
+        &self.storage[idx_cast * self.inner_dim..(idx_cast + 1) * self.inner_dim]
+    }
+}
+
+// This function has really been the bane of solving this problem in a succinct way.
+// I'm not sure what the rust way of solving this problem is but I should go look at
+// some other solutions. Perhaps more generically making this a pair of ranges with
+// step?
+type InnerBox<'a> = Box<dyn Iterator<Item = (usize, usize)> + 'a>;
+fn get_iter<'a, T>(m : &'a Matrix<T>, column_major : bool, reversed : bool) -> Box<dyn Iterator<Item = InnerBox<'a>> + 'a> {
+    let xmax = m.width();
+    let ymax = m.height();
+
+    if column_major {
+        let outer_range = 0..xmax;
+        if reversed {
+            Box::new(
+                outer_range.map(
+                    move |x|
+                        Box::new((0..ymax).rev().cartesian_product(once(x))) as InnerBox<'a>)
+            )
+        } else {
+            Box::new(
+                outer_range.map(
+                    move |x|
+                        Box::new((0..ymax).cartesian_product(once(x))) as InnerBox<'a>
+                )
+            )
+        }
+    } else {
+        let outer_range = 0..ymax;
+        if reversed {
+            Box::new(
+                outer_range.map(
+                    move |y| Box::new(once(y).cartesian_product((0..xmax).rev())) as InnerBox<'a>
+                )
+            )
+        } else {
+            Box::new(
+                outer_range.map(
+                    move |y| Box::new(once(y).cartesian_product(0..xmax)) as InnerBox<'a>
+                )
+            )
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
@@ -43,6 +113,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mat = Matrix::from_iter(input.lines().map(|x| x.chars()));
 
     println!("Initial matrix:\n{}", mat);
+
+    let mut visible: BitSet = BitSet::with_capacity(mat.num_elements());
+
+    let mut iterators = vec![];
+    {
+        for column_major in [true, false] {
+            for reversed in [true, false] {
+                iterators.push(get_iter(&mat, column_major, reversed));
+            }
+        }
+    }
+
+    let mut visible_count: usize = 0; 
+    for outer_iter in iterators {
+        for inner_iter in outer_iter {
+            let mut curr_max: Option<u32> = None;
+            for (y, x) in inner_iter {
+                let tree_size = mat[y][x];
+                if let Some(unpacked_max) = &curr_max {
+                    if tree_size > *unpacked_max {
+                        if visible.insert(y * mat.width() + x) {
+                            visible_count += 1;
+                        }
+                        curr_max = Some(tree_size);
+                    }
+                } else {
+                    if visible.insert(y * mat.width() + x) {
+                        visible_count += 1;
+                    }
+                    curr_max = Some(tree_size);
+                }
+            }
+        }
+    }
+
+    println!("Total visible trees were {}", visible_count);
 
     Ok(())
 }
