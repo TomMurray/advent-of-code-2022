457f13200b562e2ffb6096bf97b9dbdb1450b2fe
Tom Murray <tom.paul.murray@gmail.com>
2023-02-14 16:28:06 +0000

    day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,8 +1,15 @@
-use std::{env, error::Error, fs::File, io::{BufRead, BufReader}, str::FromStr, fmt};
+use std::{
+    env,
+    error::Error,
+    fmt,
+    fs::File,
+    io::{BufRead, BufReader},
+    str::FromStr,
+};
 
 enum Instruction {
     Noop,
-    Addx(i32)
+    Addx(i32),
 }
 
 #[derive(Clone, Debug)]
@@ -14,7 +21,7 @@ impl fmt::Display for InstructionParseError {
     }
 }
 
-impl Error for InstructionParseError{}
+impl Error for InstructionParseError {}
 
 impl FromStr for Instruction {
     type Err = InstructionParseError;
@@ -30,18 +37,17 @@ impl FromStr for Instruction {
                     if let Some(operand) = operand {
                         let operand = operand.parse::<i32>();
                         match operand {
-                            Ok(count) => {
-                                Ok(Instruction::Addx(count))
-                            },
-                            Err(err) => {
-                                Err(InstructionParseError(err.to_string()))
-                            }
+                            Ok(count) => Ok(Instruction::Addx(count)),
+                            Err(err) => Err(InstructionParseError(err.to_string())),
                         }
                     } else {
                         Err(InstructionParseError(String::from(s)))
                     }
-                },
-                _ => Err(InstructionParseError(format!("Unrecognised instruction '{}'", name)))
+                }
+                _ => Err(InstructionParseError(format!(
+                    "Unrecognised instruction '{}'",
+                    name
+                ))),
             }
         } else {
             Err(InstructionParseError(String::from(s)))
@@ -52,7 +58,7 @@ impl FromStr for Instruction {
 struct XReg {
     reg: i32,
     pipeline: Vec<Option<i32>>,
-    pos: usize
+    pos: usize,
 }
 
 impl XReg {
@@ -60,7 +66,7 @@ impl XReg {
         Self {
             reg: init_val,
             pipeline: vec![None; pipeline_length],
-            pos: 0
+            pos: 0,
         }
     }
 
@@ -75,7 +81,7 @@ impl XReg {
         self.pos = (self.pos + 1) % self.pipeline.len();
     }
 
-    fn issue_add(&mut self, val : i32) {
+    fn issue_add(&mut self, val: i32) {
         let plen = self.pipeline.len();
         let last_idx = (self.pos + (plen - 1)) % plen;
         self.pipeline[last_idx] = Some(val);
@@ -85,8 +91,6 @@ impl XReg {
         self.reg
     }
 }
-
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -99,15 +103,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     const PIPELINE_LEN: usize = 2;
 
     let mut xreg = XReg::new(1, PIPELINE_LEN);
-    let mut cycle : i32 = 1;
+    let mut cycle: usize = 1;
 
-    let mut res : i32 = 0;
+    let mut res: i32 = 0;
 
-    let mut capture_result = |val, cycle| {
-        res += cycle * val;
+    let mut capture_result_pt1 = |val, cycle| {
+        res += (cycle as i32) * val;
     };
 
-    for line in BufReader::new(input).lines() {        
+    const CRT_DIMS: [usize; 2] = [6, 40];
+    let num_pixels: usize = CRT_DIMS.into_iter().product();
+
+    let mut crt_storage: Vec<char> = Vec::with_capacity(num_pixels);
+
+    for line in BufReader::new(input).lines() {
         let instr: Instruction = line?.parse()?;
         let cycles = match instr {
             Instruction::Noop => 1,
@@ -118,8 +127,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         };
 
         for _ in 0..cycles {
+            // Current cycle logic goes here
+            let curr_val = xreg.get_curr_val();
+
             if ((cycle + 20) % 40) == 0 {
-                capture_result(xreg.get_curr_val(), cycle);
+                capture_result_pt1(curr_val, cycle);
+            }
+
+            // Cycle is 1-based indexing, crt_storage is 0-based
+            let cursor_x: i32 = ((cycle - 1) % CRT_DIMS[1]).try_into().unwrap();
+            let sprite_x: i32 = curr_val;
+
+            println!("cursor_x = {}, sprite_x = {}", cursor_x, sprite_x);
+
+            // See if the sprite position overlaps the cursor position
+            if cursor_x >= (sprite_x - 1) && cursor_x <= (sprite_x + 1) {
+                crt_storage.push('#');
+            } else {
+                crt_storage.push('.');
             }
 
             xreg.tick();
@@ -128,7 +153,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Print result:
-    println!("Result was {}", res);
+    println!("Part 1 result was {}", res);
+
+    // Print crt for part 2:
+    for (index, c) in crt_storage.into_iter().enumerate() {
+        if index != 0 && index % CRT_DIMS[1] == 0 {
+            println!("");
+        }
+        print!("{}", c);
+    }
 
     Ok(())
 }
